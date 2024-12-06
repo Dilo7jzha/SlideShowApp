4c79b36862c96db42542e06272804060a0828474
berniejenny <berniejenny@users.noreply.github.com>
2024-12-06 14:45:41 +1100

    Physically based material and high-quality mip-map for texture in GlobeView

--- SlideShowApp/Views/GlobeView.swift
+++ SlideShowApp/Views/GlobeView.swift
@@ -30,10 +30,9 @@ struct GlobeView: View {
         let sphere = MeshResource.generateSphere(radius: 0.2)
         do {
             let texture = try TextureResource.load(named: "globe_texture")
-            var material = SimpleMaterial()
-            material.baseColor = .texture(texture) //A warning is shown here but it's working fine
+            var material = PhysicallyBasedMaterial()
+            material.baseColor.texture = MaterialParameters.Texture(texture, sampler: Self.highQualityTextureSampler)
             globeEntity.model = ModelComponent(mesh: sphere, materials: [material])
-            
         } catch {
             print("Failed to load texture: \(error.localizedDescription)")
         }
@@ -43,5 +42,15 @@ struct GlobeView: View {
         if let position = appModel.selectedStoryPoint?.globeState?.position {
             globeEntity.transform.translation = position
         }
+    }
+    
+    /// Highest possible quality for mipmap texture sampling
+    private static var highQualityTextureSampler: MaterialParameters.Texture.Sampler {
+        let samplerDescription = MTLSamplerDescriptor()
+        samplerDescription.maxAnisotropy = 16 // 16 is maximum number of samples for anisotropic filtering (default is 1)
+        samplerDescription.minFilter = MTLSamplerMinMagFilter.linear // linear filtering (instead of nearest) when texture pixels are larger than rendered pixels
+        samplerDescription.magFilter = MTLSamplerMinMagFilter.linear // linear filtering (instead of nearest) when texture pixels are smaller than rendered pixels
+        samplerDescription.mipFilter = MTLSamplerMipFilter.linear // linear interpolation between mipmap levels
+        return MaterialParameters.Texture.Sampler(samplerDescription)
     }
 }
